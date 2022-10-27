b3bdd7d89a4cd502d76dc527e733b2620035153e
Rizal Sahroni <imperialsync@gmail.com>
2022-10-27 18:50:58 +0800

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7e1ac98f51b85e8bed401c593774ef73ada3cd07',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7e1ac98f51b85e8bed401c593774ef73ada3cd07',
+    ),
+    'abraham/twitteroauth' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51a502cde3c4f414ea0f98827afbeca1f19dfe2d',
+    ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.2.11',
+      'version' => '1.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b072d51c5a9c6f3412f7ea3ab043d6603cb2582',
+    ),
+  ),
+);
